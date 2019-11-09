8b700363ce641e45d3bb6f17bbaad7c2f690c226
Thomas Braccia <bracciata@gmail.com>
2019-11-08 21:16:21 -0600

    Progress on adding Google ML text recognition.

--- HackKState19/app/src/main/java/com/example/hackkstate19/MainActivity.java
+++ HackKState19/app/src/main/java/com/example/hackkstate19/MainActivity.java
@@ -2,7 +2,30 @@ package com.example.hackkstate19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
+import com.google.firebase.ml.vision.document.FirebaseVisionCloudDocumentRecognizerOptions;
+import com.google.firebase.ml.vision.document.FirebaseVisionDocumentText;
+import com.google.firebase.ml.vision.document.FirebaseVisionDocumentTextRecognizer;
+import com.google.firebase.ml.vision.text.FirebaseVisionCloudTextRecognizerOptions;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+import com.google.firebase.ml.vision.text.RecognizedLanguage;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +34,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    // After selecting image call the following method.
+    void processImage(Bitmap image){
+        FirebaseVisionDocumentTextRecognizer detector = getCloudDocumentRecognizer();
+        VisionImage vImg =  new VisionImage();
+        FirebaseVisionImage myImage = vImg.imageFromBitmap(image);
+        detector.processImage(myImage)
+                .addOnSuccessListener(new OnSuccessListener<FirebaseVisionDocumentText>() {
+                    @Override
+                    public void onSuccess(FirebaseVisionDocumentText result) {
+                        // Task completed successfully
+                        // ...
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Task failed with an exception
+                        // ...
+                    }
+                });
+    }
+    // Code from:
+    private FirebaseVisionDocumentTextRecognizer getCloudDocumentRecognizer() {
+        // [START mlkit_cloud_doc_recognizer]
+        // Or, to provide language hints to assist with language detection:
+        // See https://cloud.google.com/vision/docs/languages for supported languages
+        FirebaseVisionCloudDocumentRecognizerOptions options =
+                new FirebaseVisionCloudDocumentRecognizerOptions.Builder()
+                        .setLanguageHints(Arrays.asList("en", "hi"))
+                        .build();
+        FirebaseVisionDocumentTextRecognizer detector = FirebaseVision.getInstance()
+                .getCloudDocumentTextRecognizer(options);
+        // [END mlkit_cloud_doc_recognizer]
+
+        return detector;
+    }
 }
+
