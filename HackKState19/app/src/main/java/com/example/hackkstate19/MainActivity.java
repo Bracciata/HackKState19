72439ed687300c3fbe08fb7c1f0221c718aa3807
Danny <danny.tran2018@gmail.com>
2019-11-09 03:36:16 -0600

    Implementing summarization feature to MainActivity

--- HackKState19/app/src/main/java/com/example/hackkstate19/MainActivity.java
+++ HackKState19/app/src/main/java/com/example/hackkstate19/MainActivity.java
@@ -12,10 +12,12 @@ import android.graphics.Rect;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -25,6 +27,8 @@ import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.document.FirebaseVisionCloudDocumentRecognizerOptions;
@@ -38,7 +42,7 @@ import java.util.List;
 import static android.app.PendingIntent.getActivity;
 import static android.content.ContentValues.TAG;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     private static final int MY_PERMISSIONS_REQUEST_CAMERA = 0;
     private Camera mCamera;
@@ -48,17 +52,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         loadCamera();
-        // Add a listener to the Capture button
-        Button captureButton = (Button) findViewById(R.id.button_capture);
-        captureButton.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        // get an image from the camera
-                        mCamera.takePicture(null, null, mPicture);
-                    }
-                }
-        );
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.navigation_full:
+                // Open Full text read from image.
+                openFull();
+                break;
+            case R.id.navigation_summary:
+                openSummary();
+                break;
+        }
+        return true;
+    }
+
+    private void openFull() {
+        BottomNavigationView btv = findViewById(R.id.navigation);
+        btv.setSelectedItemId(R.id.navigation_full);
+        TextView outputText = findViewById(R.id.output_text);
+        outputText.setText(fullText);
+    }
+    String summary  = "";
+    String fullText="";
+    private void openSummary() {
+        BottomNavigationView btv = findViewById(R.id.navigation);
+        btv.setSelectedItemId(R.id.navigation_summary);
+        TextView outputText = findViewById(R.id.output_text);
+        outputText.setText(summary);
+
     }
 
     @Override
@@ -68,7 +92,8 @@ public class MainActivity extends AppCompatActivity {
         }
         super.onDestroy();
     }
-    void checkPermissions(){
+
+    void checkPermissions() {
         // Here, thisActivity is the current activity
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.CAMERA)
@@ -97,27 +122,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // https://developer.android.com/guide/topics/media/camera
-    /** A safe way to get an instance of the Camera object. */
-    public static Camera getCameraInstance(){
+
+    /**
+     * A safe way to get an instance of the Camera object.
+     */
+    public static Camera getCameraInstance() {
         Camera c = null;
         try {
             c = Camera.open(); // attempt to get a Camera instance
-            Log.e(TAG, "HereTommy" );
-        }
-        catch (Exception e){
+            Log.e(TAG, "HereTommy");
+        } catch (Exception e) {
             // Camera is not available (in use or does not exist)
-            Log.e(TAG, e+"HERE TOMMTY" );
+            Log.e(TAG, e + "HERE TOMMTY");
 
         }
         return c; // returns null if camera is unavailable
     }
+
     private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
 
         @Override
         public void onPictureTaken(byte[] data, Camera camera) {
             Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
 
-            if (bmp == null){
+            if (bmp == null) {
                 Log.d(TAG, "Error creating media file, check storage permissions");
                 return;
             }
@@ -125,20 +153,58 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    void openConfirmationScreen(Bitmap bmp){
+    void openConfirmationScreen(Bitmap bmp) {
         setContentView(R.layout.activity_confirmation);
         ImageView image = findViewById(R.id.imageview_confirm);
         Matrix matrix = new Matrix();
         matrix.postRotate(90);
         Bitmap scaledBitmap = Bitmap.createScaledBitmap(bmp, bmp.getWidth(), bmp.getHeight(), true);
-        Bitmap rotatedBitmap = Bitmap.createBitmap(scaledBitmap, 0, 0, scaledBitmap.getWidth(), scaledBitmap.getHeight(), matrix, true);
+        final Bitmap rotatedBitmap = Bitmap.createBitmap(scaledBitmap, 0, 0, scaledBitmap.getWidth(), scaledBitmap.getHeight(), matrix, true);
         image.setImageBitmap(rotatedBitmap);
+        Button retakeButton = (Button) findViewById(R.id.button_retake);
+        retakeButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        loadCamera();
+                    }
+                }
+        );
+        Button confirmButton = (Button) findViewById(R.id.button_confirm);
+        confirmButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Opens analysis output
+                        openOutput(rotatedBitmap);
+                    }
+                }
+        );
     }
-    void loadCamera(){
+
+    void openOutput(Bitmap bmp) {
+        // Process image
+        processImage(bmp);
+
+        setContentView(R.layout.activity_output);
+        BottomNavigationView btv = findViewById(R.id.navigation);
+        btv.setOnNavigationItemSelectedListener(MainActivity.this);
+        openSummary();
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadCamera();
+            }
+        });
+
+    }
+
+    void loadCamera() {
         setContentView(R.layout.activity_main);
-        try{
+        try {
             mCamera.release();
-        }catch (Exception e){
+        } catch (Exception e) {
 
         }
         // Create an instance of Camera
@@ -148,19 +214,32 @@ public class MainActivity extends AppCompatActivity {
         mPreview = new CameraPreview(this, mCamera);
         FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
         preview.addView(mPreview);
+        // Add a listener to the Capture button
+        Button captureButton = (Button) findViewById(R.id.button_capture);
+        captureButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // get an image from the camera
+                        mCamera.takePicture(null, null, mPicture);
+                    }
+                }
+        );
     }
+
     // After selecting image call the following method.
-    void processImage(Bitmap image){
+    void processImage(Bitmap image) {
         FirebaseVisionDocumentTextRecognizer detector = getCloudDocumentRecognizer();
-        VisionImage vImg =  new VisionImage();
+        VisionImage vImg = new VisionImage();
         FirebaseVisionImage myImage = vImg.imageFromBitmap(image);
         detector.processImage(myImage)
                 .addOnSuccessListener(new OnSuccessListener<FirebaseVisionDocumentText>() {
                     @Override
                     public void onSuccess(FirebaseVisionDocumentText result) {
                         // Task completed successfully
-                        String text = pullText(result);
+                        fullText = pullText(result);
                         //Send the above to Danny's summary code.
+                        summary = Summary.request("", fullText, 20);
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
@@ -173,27 +252,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // Code from: https://firebase.google.com/docs/ml-kit/android/recognize-text
-    String pullText(FirebaseVisionDocumentText result){
+    String pullText(FirebaseVisionDocumentText result) {
         String resultText = result.getText();
         // The following can be used for logging confidence.
         Float minConfidenceOfBlock = 1.0f;
-        for (FirebaseVisionDocumentText.Block block: result.getBlocks()) {
+        for (FirebaseVisionDocumentText.Block block : result.getBlocks()) {
             String blockText = block.getText();
             Float blockConfidence = block.getConfidence();
-            minConfidenceOfBlock = Math.min(blockConfidence,minConfidenceOfBlock);
+            minConfidenceOfBlock = Math.min(blockConfidence, minConfidenceOfBlock);
             List<RecognizedLanguage> blockRecognizedLanguages = block.getRecognizedLanguages();
             Rect blockFrame = block.getBoundingBox();
-            for (FirebaseVisionDocumentText.Paragraph paragraph: block.getParagraphs()) {
+            for (FirebaseVisionDocumentText.Paragraph paragraph : block.getParagraphs()) {
                 String paragraphText = paragraph.getText();
                 Float paragraphConfidence = paragraph.getConfidence();
                 List<RecognizedLanguage> paragraphRecognizedLanguages = paragraph.getRecognizedLanguages();
                 Rect paragraphFrame = paragraph.getBoundingBox();
-                for (FirebaseVisionDocumentText.Word word: paragraph.getWords()) {
+                for (FirebaseVisionDocumentText.Word word : paragraph.getWords()) {
                     String wordText = word.getText();
                     Float wordConfidence = word.getConfidence();
                     List<RecognizedLanguage> wordRecognizedLanguages = word.getRecognizedLanguages();
                     Rect wordFrame = word.getBoundingBox();
-                    for (FirebaseVisionDocumentText.Symbol symbol: word.getSymbols()) {
+                    for (FirebaseVisionDocumentText.Symbol symbol : word.getSymbols()) {
                         String symbolText = symbol.getText();
                         Float symbolConfidence = symbol.getConfidence();
                         List<RecognizedLanguage> symbolRecognizedLanguages = symbol.getRecognizedLanguages();
