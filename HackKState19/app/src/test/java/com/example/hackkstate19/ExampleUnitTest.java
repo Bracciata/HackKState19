4ddcf4d5ae98a47cc100d9dd3f0d71164fe15141
Danny <danny.tran2018@gmail.com>
2019-11-09 10:36:29 -0600

    Adding adjusted test case with multiple sentences.

--- HackKState19/app/src/test/java/com/example/hackkstate19/ExampleUnitTest.java
+++ HackKState19/app/src/test/java/com/example/hackkstate19/ExampleUnitTest.java
@@ -11,8 +11,8 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
-        String fullText = "Sometimes services offer paged requests. How this is done is not standardized but Unirest proves a mechanism to follow pages until all have been consumed. You must provide two functions for extracting the next page.";
-        String response = Summary.request("", "Okay", fullText, 20, -1);
-        assertEquals("You must provide two functions for extracting the next page.", response);
+        String fullText = "Deleting from an AVL tree is the same as a normal BST. Leaves can be straightforwardly deleted, nodes with a single child can have the child promoted, and nodes with both children can have their key replaced with either the maximal element in the left subtree (containing the lesser elements) or the minimal element in the right subtree (containing the greater elements). However, doing so may unbalance the tree. Deleting a node may reduce the height of some subtree which will affect the balance factor of its ancestors. The same rotations may be necessary to rebalance ancestor nodes.";
+        String response = Summary.request("", "Okay", fullText, -1, 2);
+        assertEquals("Deleting a node may reduce the height of some subtree which will affect the balance factor of its ancestors. The same rotations may be necessary to rebalance ancestor nodes.", response);
     }
 }
